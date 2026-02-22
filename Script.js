e1d2f6453324e91f8670c6fb6e04c9151f4777be
triss-web <ivan.kogoj@gmail.com>
2026-02-23 01:10:21 +1030

    Delete Script.js

--- Script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("contact-form")?.addEventListener("submit", e => {
-  e.preventDefault();
-  alert("Thanks for the message! (In real life you'd connect this to Formspree / EmailJS / Supabase / etc.)");
-  e.target.reset();
-});
